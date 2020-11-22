2f364f62b7f64d14b2920108529fe8c4be24febf
Lazemare <zjt033@gmail.com>
2020-11-22 23:56:51 +0800

    Update include.h

--- include.h
+++ include.h
@@ -15,8 +15,10 @@ struct MDSYSTEM {
 #define k_B 1.0
 
 double gasdev(void);
+void print_task_info(int pos);
 void vv2(struct MDSYSTEM *mdsystem);
 void cs4(struct MDSYSTEM *mdsystem);
+void print_help(int argc, char *argv[]);
 void update_a(struct MDSYSTEM *mdsystem);
 void update_v(struct MDSYSTEM *mdsystem);
 void update_q(struct MDSYSTEM *mdsystem);
@@ -25,7 +27,5 @@ void update_E(struct MDSYSTEM *mdsystem);
 void update_T(struct MDSYSTEM *mdsystem);
 void update_Ek(struct MDSYSTEM *mdsystem);
 void update_Ep(struct MDSYSTEM *mdsystem);
-void thermostat_lan(struct MDSYSTEM *mdsystem, double tau, double temp_target);
-void print_task_info(int pos);
-void print_help(int argc, char *argv[]);
 void print_data(struct MDSYSTEM mdsystem, int step);
+void thermostat_lan(struct MDSYSTEM *mdsystem, double tau, double temp_target);
